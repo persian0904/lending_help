c7190f27a555fba021cbba7ec8ec2781aecf7bd9
TTF SOLUTION <persian0904@gmail.com>
2020-11-10 17:26:07 +0700

    Update for developer
    Update for developer

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/NghiemTuan/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/NghiemTuan/Desktop/TempData/Android/flutter_wallet_ui-master"
-export "FLUTTER_TARGET=/Users/NghiemTuan/Desktop/TempData/Android/flutter_wallet_ui-master/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/NghiemTuan/Desktop/TempData/Android/Wallet-app-UI-master"
+export "FLUTTER_TARGET=/Users/NghiemTuan/Desktop/TempData/Android/Wallet-app-UI-master/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,15 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_wallet_ui_challenge/src/app.dart';
 
-
-
+import 'package:payme/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
